4557c718631fd699d7425f4fa0c3cb774d3320c2
Renan <Renan@DESKTOP-LV886FA>
2018-03-20 22:17:28 -0300

    rest

---
+++ SistemaHospedagemArqSoft/src/br/edu/up/SistemaHospedagemArqSoft/rest/ClienteRest.java
@@ -0,0 +1,34 @@
+package br.edu.up.SistemaHospedagemArqSoft.rest;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.edu.up.SistemaHospedagemArqSoft.entity.Cliente;
+import br.edu.up.SistemaHospedagemArqSoft.facade.HospedagemFacade;
+import br.edu.up.SistemaHospedagemArqSoft.service.ServiceException;
+
+@Path("/clienteController")
+public class ClienteRest {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Cliente> listar(){
+		return new HospedagemFacade().listar();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void salvar (Cliente cliente) {
+		try {
+			new HospedagemFacade().salvarCliente(cliente);
+		} catch (ServiceException e) {
+			e.printStackTrace();
+		}
+	}
+}
